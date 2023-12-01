58bfc98648cbf9c92b9099154613dc27e49e3d94
root <root@master.example.com>
2023-12-01 14:16:44 +0000

    Shared Lib Class - Demo

---
+++ src/com/jenkinsshlibdemo/Car.groovy
@@ -0,0 +1,22 @@
+package com.jenkinsshlibdemo;
+
+
+class Car {
+    int numberOfDoors
+    String brand
+    String model
+    def script 
+
+    Car(script) {
+      this.script = script
+    }
+
+
+   void printOurCar() {
+      script.echo "Using the script.echo"
+      script.echo "numberOfDoors:${numberOfDoors}"
+      script.echo "brand:${brand}"
+      script.echo "model.${model}"
+
+   }
+}
